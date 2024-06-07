9b28c0c05f46bab4939b30d5441790bf945d6936
AakashKainth <aakash.kainth@gmail.com>
2024-06-07 16:29:37 -0400

    If else, Switch and For loop

---
+++ src/app/structure-directives/structure-directives.component.ts
@@ -0,0 +1,25 @@
+import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-structure-directives',
+  standalone: true,
+  imports: [NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, NgFor],
+  templateUrl: './structure-directives.component.html',
+  styleUrl: './structure-directives.component.css'
+})
+export class StructureDirectivesComponent {
+
+public fruits = ["Banana","Mango","Grapes"];
+public persons = [
+     {name:"Kumar", age:24, isMArried: true},
+     {name:"Zain", age:26, isMArried: false},
+     {name:"Aakash", age:21, isMArried: true}
+   ];
+
+  //public pizza = 'Supreme';
+
+    // public display = false;
+    // public fruit = 'Banana';
+}
