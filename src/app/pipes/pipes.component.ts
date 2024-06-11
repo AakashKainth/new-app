a89ebe097701736f797bee51d7ca5104960a5bab
AakashKainth <aakash.kainth@gmail.com>
2024-06-11 12:32:25 -0400

    Added Date pipe and its formats

--- src/app/pipes/pipes.component.ts
+++ src/app/pipes/pipes.component.ts
@@ -1,21 +1,25 @@
-import { JsonPipe, LowerCasePipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { DatePipe, JsonPipe, LowerCasePipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-pipes',
   standalone: true,
-  imports: [UpperCasePipe, LowerCasePipe, TitleCasePipe, SlicePipe, JsonPipe],
+  imports: [UpperCasePipe, LowerCasePipe, TitleCasePipe, SlicePipe, JsonPipe, DatePipe],
   templateUrl: './pipes.component.html',
   styleUrl: './pipes.component.css'
 })
 export class PipesComponent {
   public channelName = "learning never ends";
   public name ="AAKASH";
+  public myDate = new Date();
+  months =["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+     "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
   public person = {
     name: "Kumar",
     age: 23,
     height: 5.10
   }
+  
 
 }
